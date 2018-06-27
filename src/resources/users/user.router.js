0c76244abe59fd5ea7721ba2762f3cfe6980d4ad
Flavio Oliveira <flavio@foli.sk>
2018-06-27 18:23:56 +0200

    Lecture 4.20 - Model Sanitization

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { sanitizeBody } from 'express-validator/filter'
 import userController from './user.controller'
 export const userRouter = express.Router()
 
@@ -7,5 +8,21 @@ userRouter.route('/').get(userController.getUsers)
 userRouter
 	.route('/:id')
 	.get(userController.getProfile)
-	.put(userController.updateUser)
+	.put(
+		[
+			sanitizeBody('email')
+				.trim()
+				.escape(),
+			sanitizeBody('username')
+				.trim()
+				.escape(),
+			sanitizeBody('bio')
+				.trim()
+				.escape(),
+			sanitizeBody('url')
+				.trim()
+				.escape()
+		],
+		userController.updateUser
+	)
 	.delete(userController.deleteUser)
--- src/router.js
+++ src/router.js
@@ -2,11 +2,20 @@ import express from 'express'
 export const router = express.Router()
 import { userRouter } from './resources/users/user.router'
 import userController from './resources/users/user.controller'
+import { sanitizeBody } from 'express-validator/filter'
 
 router.get('/', (req, res) => res.send('Home Page'))
 router.get('/about', (req, res) => res.send('About Page'))
 
 router.get('/signin', (req, res) => res.send('Sign In'))
-router.post('/signup', userController.createUser)
+router.post(
+	'/signup',
+	[
+		sanitizeBody('email')
+			.trim()
+			.escape()
+	],
+	userController.createUser
+)
 
 router.use('/users', userRouter)
