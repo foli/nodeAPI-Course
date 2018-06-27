f6c41fa7dd534087c0e0304926339ca19b93872d
Flavio Oliveira <flavio@foli.sk>
2018-06-27 09:09:52 +0200

    Lecture 2.4 - App Structure

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 
 // Set options as a parameter, environment variable, or rc file.
 require = require("esm")(module/*, options*/)
-module.exports = require("./main.js")
+module.exports = require("./src/app.js")
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
 
-app.get('/', (req, res) => res.send('It works'))
+app.get('/', (req, res) => res.send('Welcome to Express'))
 
 app.listen(8080, () => console.log('Server is running on http://localhost:8080'))
