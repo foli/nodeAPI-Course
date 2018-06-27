95563cb45d194ed1363189ea781bb448ce506026
Flavio Oliveira <flavio@foli.sk>
2018-06-27 09:26:46 +0200

    Lecture 2.5 - Code Quality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-"use strict"
+'use strict'
 
 // Set options as a parameter, environment variable, or rc file.
-require = require("esm")(module/*, options*/)
-module.exports = require("./src/app.js")
+require = require('esm')(module /*, options*/)
+module.exports = require('./src/app.js')
--- src/app.js
+++ src/app.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Welcome to Express'))
 
-app.listen(8080, () => console.log('Server is running on http://localhost:8080'))
+app.listen(8080, () =>
+	console.log('Server is running on http://localhost:8080')
+)
